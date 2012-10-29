c8759c1e2db68d3c2302274bcb2f760678fd7158
LeChosenOne <jonahw@optonline.net>
2012-10-29 00:22:54 -0300

    /vote added

--- fCraft/Commands/Command Handlers/VoteHandler.cs
+++ fCraft/Commands/Command Handlers/VoteHandler.cs
@@ -1,4 +1,4 @@
-﻿//Copyright (C) <2012>  <Jon Baker, Glenn Mariën and Lao Tszy>
+//Copyright (C) <2012>  <Jon Baker, Glenn Mariën and Lao Tszy>
 
 //This program is free software: you can redistribute it and/or modify
 //it under the terms of the GNU General Public License as published by
@@ -38,7 +38,7 @@ namespace fCraft
 
         public static void NewVote()
         {
-            Usage = "&A/Vote Yes | No | Ask | Kick | Abort";
+            Usage = "&A/Vote Yes | No | Ask | Abort";
             VotedYes = 0;
             VotedNo = 0;
             Voted = new List<Player>();
@@ -112,73 +112,6 @@ namespace fCraft
                     player.Message("&8You have voted for 'Yes'");
                     break;
 
-                case "kick":
-                    string toKick = cmd.Next();
-                    string Reason = cmd.NextAll();
-                    VoteKickReason = Reason;
-                    if (toKick == null)
-                    {
-                        player.Message("Target cannot be empty. " + Usage);
-                        return;
-                    }
-
-                    Player target = Server.FindPlayerOrPrintMatches(player, toKick, false, true);
-
-                    if (target == null)
-                    {
-                        // FIX: Target is null when no such player is online, this caused crashes
-                        return;
-                    }
-
-                    if (!Player.IsValidName(target.Name))
-                    {
-                        return;
-                    }
-
-                    if (!player.Can(Permission.MakeVoteKicks))
-                    {
-                        player.Message("You do not have permissions to start a VoteKick");
-                        return;
-                    }
-
-                    if (VoteIsOn)
-                    {
-                        player.Message("A vote has already started. Each vote lasts 1 minute.");
-                        return;
-                    }
-
-                    if (VoteKickReason.Length < 3)
-                    {
-                        player.Message("Invalid reason");
-                        return;
-                    }
-
-                    if (target == player)
-                    {
-                        player.Message("You cannot VoteKick yourself, lol");
-                        return;
-                    }
-
-                    VoteThread = new Thread(new ThreadStart(delegate
-                      {
-                          TargetName = target.Name;
-                          if (!Player.IsValidName(TargetName))
-                          {
-                              player.Message("Invalid name");
-                              return;
-                          }
-                          NewVote();
-                          VoteStarter = player.ClassyName;
-                          Server.Players.Message("{0}&S started a VoteKick for player: {1}", player.ClassyName, target.ClassyName);
-                          Server.Players.Message("&WReason: {0}", VoteKickReason);
-                          Server.Players.Message("&9Vote now! &S/Vote &AYes &Sor /Vote &CNo");
-                          VoteIsOn = true;
-                          Logger.Log(LogType.SystemActivity, "{0} started a votekick on player {1} reason: {2}", player.Name, target.Name, VoteKickReason);
-                          Thread.Sleep(60000);
-                          VoteKickCheck();
-                      })); VoteThread.Start();
-                    break;
-
                 case "no":
                     if (!VoteIsOn)
                     {
@@ -243,52 +176,8 @@ namespace fCraft
             }
         }
 
-        static void VoteKickCheck()
-        {
-            if (VoteIsOn)
-            {
-                Server.Players.Message("{0}&S wanted to get {1} kicked. Reason: {2} \n&SResults are in! Yes: &A{3} &SNo: &C{4}", VoteStarter,
-                                      TargetName, VoteKickReason, VotedYes, VotedNo);
 
-                Player target = null;
-
-                try
-                {
-                    target = Server.FindPlayerOrPrintMatches(Player.Console, TargetName, true, true);
-                }
-                catch (Exception ex)
-                {
-                    // Log exception
-                    Logger.Log(LogType.Error, ex.Message);
-
-                    // Notify users
-                    Server.Message("Unexpected error while trying to execute votekick");
-
-                    // Unable to find target so quit.
-                    VoteIsOn = false;
-                    TargetName = null;
-                    return;
-                }
-
-                if (target == null)
-                {
-                    Server.Message("{0}&S is offline", target.ClassyName);
-                    return;
-                }
-                else if (VotedYes > VotedNo)
-                {
-                    Scheduler.NewTask(t => target.Kick(Player.Console, "VoteKick by: " + VoteStarter + " - " + VoteKickReason, LeaveReason.Kick, false, true, false)).RunOnce(TimeSpan.FromSeconds(3));
-                    Server.Players.Message("{0}&S was kicked from the server", target.ClassyName);
-                }
-                else
-                    Server.Players.Message("{0} &Sdid not get kicked from the server", target.ClassyName);
-                VoteIsOn = false;
-                TargetName = null;
-                foreach (Player V in Voted)
-                {
-                    V.Info.HasVoted = false;
-                }
-            }
-        }
     }
 }
+
+
