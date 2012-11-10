90be25c701f60bb47e822dd198ac302229850f99
DingusBungus <themantimes8@aol.com>
2012-11-10 18:47:46 -0500

    Changed bug report URL

--- fCraft/System/Logger.cs
+++ fCraft/System/Logger.cs
@@ -27,7 +27,7 @@ namespace fCraft
         const string DefaultLogFileName = "LegendCraft.log",
                      LongDateFormat = "yyyy'-'MM'-'dd'_'HH'-'mm'-'ss",
                      ShortDateFormat = "yyyy'-'MM'-'dd";
-        static readonly Uri CrashReportUri = new Uri(null);
+        static readonly Uri CrashReportUri = new Uri(http://dingusserver.forumotion.com/f9-legendcraft);
         public static LogSplittingType SplittingType = LogSplittingType.OneFile;
 
         static readonly string SessionStart = DateTime.Now.ToString(LongDateFormat); // localized
